e69722cced95a4ecf08120bb3691c4e9be157892
John Padilla <johnpadi8790@gmail.com>
2025-03-20 23:17:10 -0600

    Setting up the Foundation with UITabBarController

--- AppStoreJSON/BaseTabBarController.swift
+++ AppStoreJSON/BaseTabBarController.swift
@@ -13,8 +13,29 @@ class BaseTabBarController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "Apps"
         
-        view.backgroundColor = .red
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.tabBarItem.image = UIImage(systemName: "newspaper.fill")
+        redNavController.navigationBar.prefersLargeTitles = true
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "Search"
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        blueNavController.navigationBar.prefersLargeTitles = true
+
+        
+        viewControllers = [
+            redNavController,
+            blueNavController
+        ]
     }
 
 
